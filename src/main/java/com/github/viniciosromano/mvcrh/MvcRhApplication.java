146ce8378a3c592890bca0161627637ba8bf5311
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 19:46:48 -0300

    first commit

---
+++ src/main/java/com/github/viniciosromano/mvcrh/MvcRhApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciosromano.mvcrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRhApplication.class, args);
+	}
+
+}

5e9ec602ae616f0ece0fa28cec4b5b78f243b6ea
logonrmlocal <vinicios.romano@outloo.com>
2024-05-10 22:37:28 -0300

    UP

--- src/main/java/com/github/viniciosromano/mvc_rh/controller/FuncionarioController.java
+++ src/main/java/com/github/viniciosromano/mvc_rh/controller/FuncionarioController.java
@@ -8,21 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("funcionarios")
 public class FuncionarioController {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
+
     @GetMapping()
     public String list(Model model) {
         List<Funcionario> funcionarios = funcionarioRepository.findAll();
         model.addAttribute("funcionarios", funcionarios);
         return "funcionario/list";
     }
+
     @PostMapping("save")
-    public String save(@ModelAttribute Funcionario funcionario){
+    public String save(@ModelAttribute Funcionario funcionario) {
         funcionarioRepository.save(funcionario);
 
         return "redirect:/funcionarios";
@@ -30,18 +31,19 @@ public class FuncionarioController {
 
     @GetMapping("add")
     public String create(Model model) {
-        model.addAttribute("funcionario",new Funcionario());
+        model.addAttribute("funcionario", new Funcionario());
         return "funcionario/form";
     }
 
     @GetMapping("update/{id}")
-    public String update(@PathVariable Long id,Model model){
+    public String update(@PathVariable Long id, Model model) {
         Funcionario funcionario = funcionarioRepository.findById(id).orElse(new Funcionario());
-        model.addAttribute("funcionario",funcionario);
+        model.addAttribute("funcionario", funcionario);
         return "funcionario/form";
     }
+
     @GetMapping("delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         funcionarioRepository.deleteById(id);
         return "redirect:/funcionarios";
     }

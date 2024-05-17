6fb2ccc4a700df7a714274df0f169433a797c91c
logonrmlocal <vini@cac>
2024-05-17 20:00:55 -0300

    UP

---
+++ src/main/resources/scripts/data-dev.sql
@@ -0,0 +1,5 @@
+INSERT INTO funcionarios (id,nome_funcionario,data_entrada) values (nextval('funcionarios_seq'),'Pelé','1950-12-31');
+
+INSERT INTO funcionarios (id,nome_funcionario,data_entrada) values (nextval('funcionarios_seq'),'Pelé','1950-12-31');
+
+INSERT INTO funcionarios (id,nome_funcionario,data_entrada) values (nextval('funcionarios_seq'),'Pelé','1950-12-31');
